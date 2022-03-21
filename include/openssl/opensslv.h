36fa1953fd39b448ccbba647d298103382b5b5e8
Balu Gajjala <bagajjal@microsoft.com>
2022-03-21 13:56:40 -0700

    Upgrade to V3.4.3

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -1,11 +1,11 @@
-/* $OpenBSD: opensslv.h,v 1.66 2021/09/15 17:14:26 tb Exp $ */
+/* $OpenBSD$ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3040200fL
+#define LIBRESSL_VERSION_NUMBER 0x3040300fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.4.2"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.4.3"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
