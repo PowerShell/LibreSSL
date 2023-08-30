2048a3a1aae8ac18d0bfaf97c6b385ddd9e0c61b
Tess Gauthier <tgauth@bu.edu>
2023-08-30 12:52:12 -0400

    bump to 3.7.3 (#22)

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -3,9 +3,9 @@
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3070200fL
+#define LIBRESSL_VERSION_NUMBER 0x3070300fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.7.2"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.7.3"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
